2b6ad8ff4fbc1578afdae056ed043239851eba77
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-19 08:30:17 +0530

    Create password.py

---
+++ Shuyaa/password.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+def generate_password(length=10):
+    if length < 8:
+        print("Length should be at least 8")
+        return None
+    else:
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(characters) for i in range(length))
+        return password
+
+# Test the function
+print(generate_password(10))
