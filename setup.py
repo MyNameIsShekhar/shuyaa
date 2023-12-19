6b8480ccac2fda7dccdd1d2ccd906bf6f1df0f49
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-19 08:33:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='Shuyaa',  # Name of your package
-    version='0.1',  # Version number
-    description='A Python module to generate random passwords',  # Short description
-    url='http://github.com/MyNameIsShekhar/shuyaa',  # Link to your GitHub repository or other source
-    author='Shekhar',  # Your name
+    name='Shuyaa',  
+    version='0.1',  
+    description='A Python module to generate random passwords',  
+    url='http://github.com/MyNameIsShekhar/shuyaa',
+    author='Shekhar',  
     author_email='shekharhatture20@gmail.com',  # Your email
-    license='MIT',  # License type
-    packages=['random_password'],  # Name of directory containing __init__.py
+    license='MIT',  
+    packages=['Shuyaa'],
     install_requires=[
         'string',
         'random',
-    ],  # List of dependencies
-    zip_safe=False  # Not necessary, but can be True or False depending on your needs
+    ],  
+    zip_safe=False 
 )
 
